1e117979ba2092b26bef28b7c14754b8864a18da
Diego Monsalve <diales1981@gmail.com>
2023-08-28 14:20:04 -0500

    Adicionando páginas legales

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -6,8 +6,52 @@ function PolicyPage() {
     <Layout title='dialmonsalve | Políticas' pageDescription='Esta es la página acerca de políticas del portafolio de Diego Monsalve' >
       <h1>Políticas de privacidad</h1>
       <section className='section-policy' >
+
         <h4>Aviso de privacidad</h4>
-        <p>El Aviso de Privacidad es el documento, en formato físico, electrónico o cualquier otro medio conocido o por conocer, que se pone a disposición del titular para el tratamiento de sus datos personales. A través de este documento, se informa al titular sobre las políticas de tratamiento de datos que aplicarán, cómo acceder a ellas y las características del tratamiento de los datos personales.</p>
+
+        <p>Me comprometo a cumplir con todas las leyes y regulaciones aplicables en relación con la privacidad y protección de sus datos personales. Valoro enormemente la confianza que ha depositado en mi al elegirme como su proveedor de servicios, me esforzaré constantemente por mantener altos estándares de seguridad y privacidad.</p>
+
+        <p>Si en algún momento surge alguna pregunta o inquietud con respecto a nuestra Política de Privacidad o el tratamiento de sus datos personales, le invitamos a que se comunique con nosotros utilizando los canales de contacto que se proporcionan en este sitio web.</p>
+
+        <p>Agradezco profundamente su confianza y espero poder ofrecerle una experiencia segura y satisfactoria.</p>
+
+        <h4>Políticas de Información</h4>
+
+        <li><span>Derechos de Usuarios:</span> Recolecto datos personales a través del sitio web, formularios, solicitudes y suscripciones. Esto incluye nombre, correo, teléfono y preferencias. Podemos requerir más datos para pedidos.</li>
+
+        <li><span>Privacidad y Seguridad:</span>Protejo la privacidad de usuarios con medidas técnicas y legales. Cumplimos normativas de protección de datos.</li>
+
+        <li><span>Conservación de Datos: </span>Mantenemos datos el tiempo necesario, salvo requerimientos legales.</li>
+
+
+        <li><span>Conservación de Información:</span>Mantengo la información personal de los usuarios por el tiempo necesario para cumplir con los propósitos para los que fue recolectada, a menos que exista una obligación legal o reglamentaria que requiera su conservación durante un período más prolongado.</li>
+
+        <li><span>Consentimiento del Usuario:</span>Al proporcionar datos, usuarios aceptan términos. Pueden cancelar suscripciones en correos promocionales.</li>
+
+        <li><span>Actualizaciones de la Política:</span>Podemos cambiar la política con notificación y efecto desde la fecha de publicación. Usuarios deben revisarla periódicamente.</li>
+
+        <h4>Cookies</h4>
+        <li><span>Cookies y Tecnologías de Seguimiento:</span> Las cookies son archivos de texto en el dispositivo del usuario al visitar el sitio web, mejorando su funcionamiento. Tecnologías de seguimiento como balizas web y píxeles recopilan datos de la interacción del usuario.</li>
+
+        <li><span>Tipos de Cookies:</span>Empleamos cookies de sesión (borradas al cerrar el navegador) y cookies persistentes (almacenadas hasta eliminación manual o expiración).</li>
+
+        <li><span>Usos de las Cookies: </span>Sirven para:
+
+          <li>Personalizar experiencia y recordar preferencias.</li>
+          <li>Mejorar funcionalidad y rendimiento del sitio.</li>
+          <li>Recopilar información estadística y analítica.</li>
+          <li>Brindar publicidad personalizada.</li>
+          <li>Facilitar inicio de sesión y autenticación.</li>
+          <li>Fortalecer seguridad del sitio.</li>
+        </li>
+
+        <li><span>Control de Cookies:</span> Usuarios pueden ajustar preferencias en su navegador, pero esto puede afectar funcionalidad del sitio.</li>
+
+        <li><span>Consentimiento del Usuario:</span> Al usar el sitio, usuarios aceptan el uso de cookies y tecnologías de seguimiento.</li>
+
+        <li><span>Terceros y Cookies de Terceros:</span> Servicios de terceros pueden usar cookies y tecnologías similares, con políticas de privacidad propias.</li>
+
+        <li><span>Actualizaciones:</span> Puedo modificar esta política en cualquier momento, con notificación y efecto desde la fecha de publicación. Se recomienda revisarla periódicamente.</li>
 
         <h4>Consultas</h4>
         <p>
@@ -21,6 +65,18 @@ function PolicyPage() {
 
         <li>Utilizar servicios de atención al cliente o sistemas de reclamación en funcionamiento. En todos los casos, las solicitudes de consulta se atenderán en un plazo máximo de diez (10) días hábiles a partir de la fecha de recepción. Si no es posible atender la solicitud dentro de este plazo, se notificará al interesado antes de que se cumplan los 10 días, explicando la demora y señalando la fecha límite para atender la consulta. En ningún caso, esta fecha podrá ser posterior a cinco (5) días hábiles después de la expiración del primer plazo.
         </li>
+
+        <h4>Datos sensibles</h4>
+
+        <li><span>Recopilación Limitada:</span> Solo recolecto datos sensibles si es esencial y legal. Requiero el consentimiento explícito del titular, a menos que haya excepciones legales.</li>
+
+        <li><span>Medidas Adicionales de Seguridad:</span> Implemento seguridad extra para proteger datos sensibles, como encriptación y restricciones de acceso.</li>
+
+        <li><span>Finalidad y Uso Limitado:</span> Uso datos sensibles solo para fines específicos y legales, con el consentimiento del titular.</li>
+
+        <li><span>Conservación y Eliminación:</span> Mantengo datos sensibles el tiempo necesario, y los eliminamos de manera segura cuando ya no son requeridos.</li>
+
+        <li><span>Compartir y Transferir:</span> No comparto ni transfiero datos sensibles a terceros sin el consentimiento del titular, a menos que haya bases legales o excepciones.</li>
 
         <h4>Reclamos</h4>
         <p>Los titulares o sus representantes legales pueden presentar reclamos si consideran que la información en una base de datos debe ser corregida, actualizada o eliminada, o si detectan un posible incumplimiento de los deberes establecidos en la Ley. Estos reclamos deben ser dirigidos al Responsable del Tratamiento y canalizados a través del canal designado, cuyos detalles de contacto se proporcionarán más adelante. Dicho canal será responsable de la protección de datos personales dentro de diegomonsalve.vercel.app. El reclamo puede ser presentado por el titular, siguiendo los lineamientos del artículo 15 de la Ley 1581 de 2012 y el Decreto 1377 de 2013, y otras normativas pertinentes.</p>
@@ -44,4 +100,4 @@ function PolicyPage() {
   )
 }
 
-export default PolicyPage
+export default PolicyPage;
