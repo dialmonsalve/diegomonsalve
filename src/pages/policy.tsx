99737c1a061c741dd58e18ac496faab452076e1d
Diego Monsalve <diales1981@gmail.com>
2023-08-28 12:39:15 -0500

    Adicionando páginas legales

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -1,12 +1,47 @@
 import { Layout } from '@/components/layout';
 
 
-function PolicyPage  ()  {
+function PolicyPage() {
   return (
     <Layout title='dialmonsalve | Políticas' pageDescription='Esta es la página acerca de políticas del portafolio de Diego Monsalve' >
-      <h1>Conoce más quien soy</h1>
+      <h1>Políticas de privacidad</h1>
+      <section className='section-policy' >
+        <h4>Aviso de privacidad</h4>
+        <p>El Aviso de Privacidad es el documento, en formato físico, electrónico o cualquier otro medio conocido o por conocer, que se pone a disposición del titular para el tratamiento de sus datos personales. A través de este documento, se informa al titular sobre las políticas de tratamiento de datos que aplicarán, cómo acceder a ellas y las características del tratamiento de los datos personales.</p>
+
+        <h4>Consultas</h4>
+        <p>
+          Los titulares o sus representantes legales tienen el derecho de consultar la información personal que se encuentre en bases de datos. En este sentido, Diego Monsalve asegura el derecho de consulta proporcionando al titular toda la información contenida en su registro individual o relacionada con su identificación. Para atender solicitudes de consulta,Diego Monsalve se compromete a:
+        </p>
+
+        <li>
+          Proporcionar medios electrónicos u otros canales relevantes.</li>
+
+        <li>Establecer formularios, sistemas y métodos simplificados, indicados en el aviso de privacidad.</li>
+
+        <li>Utilizar servicios de atención al cliente o sistemas de reclamación en funcionamiento. En todos los casos, las solicitudes de consulta se atenderán en un plazo máximo de diez (10) días hábiles a partir de la fecha de recepción. Si no es posible atender la solicitud dentro de este plazo, se notificará al interesado antes de que se cumplan los 10 días, explicando la demora y señalando la fecha límite para atender la consulta. En ningún caso, esta fecha podrá ser posterior a cinco (5) días hábiles después de la expiración del primer plazo.
+        </li>
+
+        <h4>Reclamos</h4>
+        <p>Los titulares o sus representantes legales pueden presentar reclamos si consideran que la información en una base de datos debe ser corregida, actualizada o eliminada, o si detectan un posible incumplimiento de los deberes establecidos en la Ley. Estos reclamos deben ser dirigidos al Responsable del Tratamiento y canalizados a través del canal designado, cuyos detalles de contacto se proporcionarán más adelante. Dicho canal será responsable de la protección de datos personales dentro de diegomonsalve.vercel.app. El reclamo puede ser presentado por el titular, siguiendo los lineamientos del artículo 15 de la Ley 1581 de 2012 y el Decreto 1377 de 2013, y otras normativas pertinentes.</p>
+
+        <h4>Procedimiento para Ejercer Derechos</h4>
+        <p>En cualquier momento y sin costo, el titular o su representante pueden solicitar a diegomonsalve.vercel.app la corrección, actualización o eliminación de sus datos personales. Se requerirá acreditación de identidad y la solicitud puede ser enviada por correo electrónico a dialmonsalve1981@gemail.com, con el asunto: `Tratamiento Habeas Data`. Los derechos de corrección, actualización o eliminación pueden ser ejercidos por:</p>
+
+
+        <li>El titular o sus representantes, con prueba de identidad o medios electrónicos que permitan la identificación.</li>
+
+        <li>El representante legal, con prueba de representación.</li>
+
+        <li>Si la solicitud proviene de un tercero, se debe acreditar la autorización o mandato correspondiente. Si no se acredita la autorización, la solicitud se considerará inválida. La solicitud debe incluir:</li>
+
+        <li>Nombre y datos de contacto del titular para recibir la respuesta.</li>
+        <li>Documentos que acrediten la identidad o representación.</li>
+        <li>Descripción clara y precisa de los datos a rectificar, actualizar o eliminar.</li>
+
+      </section>
     </Layout>
   )
 }
 
-export  default PolicyPage
+export default PolicyPage
