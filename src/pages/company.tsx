e551a05c3c1cd8f6faa43c60bbcd5c2033399520
Diego Monsalve <diales1981@gmail.com>
2023-08-28 17:08:32 -0500

    Adicionando misión y visión

--- src/pages/company.tsx
+++ src/pages/company.tsx
@@ -1,12 +1,54 @@
 import { Layout } from '@/components/layout';
 
 
-function CompanyPage  ()  {
+function CompanyPage() {
   return (
     <Layout title="" pageDescription='Esta es la página acerca de nosotros del portafolio de Diego Monsalve' >
       <h1>Mi compañía</h1>
+
+      <section className='section-company'>
+        <h4>Misión</h4>
+
+        <p>
+          Somos una empresa apasionada por la creación, implementación y mantenimiento de sitios web que generan impacto para emprendedores, pequeñas y medianas empresas. Nuestra misión es impulsar la presencia digital de nuestros clientes, ofreciéndoles soluciones que brinden una experiencia excepcional a sus usuarios.
+        </p>
+        <p>
+          Nos destacamos por nuestro enfoque en la excelencia, trabajando incansablemente para asegurar que cada proyecto refleje la visión y los valores de nuestros clientes. A través del desarrollo de soluciones de comercio electrónico, abrimos las puertas a oportunidades de ventas en línea fluidas, potenciando la presencia y alcance de sus productos.
+        </p>
+
+        <p>
+          No solo nos esforzamos por el éxito comercial, sino que también abrazamos un fuerte compromiso social y ambiental. Promovemos activamente prácticas sostenibles, asegurando que nuestras operaciones sean respetuosas con el medio ambiente y fomentando hábitos que contribuyan a un futuro más sustentable.
+        </p>
+        <p> La tecnología es el núcleo de nuestro trabajo, y nos enorgullecemos de basar nuestras soluciones en React, Node.js y Javascript, lo que garantiza que nuestros clientes estén respaldados por la vanguardia tecnológica. Nuestro equipo se mantiene al tanto de las últimas tendencias y avances para asegurarnos de que las soluciones que brindamos estén siempre a la par con las tecnologías emergentes.  </p>
+
+        <p>
+          En última instancia, nuestro propósito es ser un socio confiable y orientado a resultados para nuestros clientes. Trabajamos en estrecha colaboración con ellos para transformar sus objetivos en realidades digitales, estableciendo estándares de calidad que superen expectativas y dejen una huella duradera en el mundo en línea.
+        </p>
+
+        <h4>Visión</h4>
+
+        <p>
+        Para el año 2025, aspiramos a ser una de las 20 empresas más reconocidas en el área metropolitana de Medellín, no solo por nuestra excelencia operacional y nuestras soluciones tecnológicas de vanguardia, sino también por nuestra continua innovación y compromiso con el bienestar de nuestros clientes y comunidad
+          </p>
+          <p>
+          Nuestra visión va más allá de ser simplemente un líder en el mercado. Buscamos mantenernos a la vanguardia de las tendencias tecnológicas emergentes, asegurando que nuestras soluciones sean siempre relevantes y efectivas para nuestros clientes en un mundo en constante evolución. Nos esforzamos por ser un faro de innovación y adaptabilidad, respondiendo a las necesidades cambiantes de manera ágil y efectiva.
+          </p>
+          <p>
+          Sin embargo, nuestro impacto no se limita a la tecnología. También estamos comprometidos a fomentar un sentido humano y empático en todo lo que hacemos. Creemos que la tecnología debe mejorar la vida de las personas y la sociedad en general. Nos enorgullece no solo brindar soluciones, sino también crear conexiones significativas y duraderas con nuestros clientes y la comunidad en la que operamos.
+          </p>
+
+          <p>
+          A medida que avanzamos hacia nuestro objetivo, continuaremos promoviendo prácticas éticas y sostenibles, apoyando la educación tecnológica y fomentando la diversidad e inclusión en nuestro equipo y en el ecosistema tecnológico. Nuestra visión no solo es ser una empresa líder, sino también ser un agente de cambio positivo en el panorama empresarial y social.
+          </p>
+
+          <p>
+          Estamos comprometidos con este camino y trabajaremos incansablemente para hacer realidad esta visión compartida, impulsando la transformación digital, el crecimiento empresarial y el bienestar colectivo en nuestra comunidad y más allá.
+          </p>
+
+      </section>
+
     </Layout>
   )
 }
 
-export  default CompanyPage
+export default CompanyPage
